bb63dabd1643928219f79cf081b837de9d1ee83c
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-05-18 15:22:43 +0700

    1

---
+++ build.nadle.ts
@@ -0,0 +1,9 @@
+import { tasks, ExecTask, PnpmTask } from "nadle";
+
+tasks.register("clean", PnpmTask, { args: ["-r", "exec", "rimraf", "lib"] });
+tasks.register("build", PnpmTask, { args: ["run", "-r", "build"] }).config({ dependsOn: ["check"] });
+tasks.register("test", PnpmTask, { args: ["run", "-r", "test"] }).config({ dependsOn: ["build"] });
+
+tasks.register("eslint", PnpmTask, { args: ["eslint"] });
+tasks.register("prettier", ExecTask, { command: "prettier", args: ["--check", "."] });
+tasks.register("check", () => console.log("Checking...")).config({ dependsOn: ["eslint", "prettier"] });
--- packages/nadle/test/fixtures/sample-app/pnpm-task.nadle.ts
+++ packages/nadle/test/fixtures/sample-app/pnpm-task.nadle.ts
@@ -1,5 +1,5 @@
 import { tasks, PnpmTask } from "../../../lib/index.js";
 
-tasks.register("pass", PnpmTask, { args: ["exec", "tsc", "./src/pass.ts", "--pretty"] });
-tasks.register("fail", PnpmTask, { args: ["exec", "tsc", "./src/fail.ts", "--pretty"] });
+tasks.register("pass", PnpmTask, { args: ["exec", "tsc", "./src/pass.ts", "--noEmit", "--pretty"] });
+tasks.register("fail", PnpmTask, { args: ["exec", "tsc", "./src/fail.ts", "--noEmit", "--pretty"] });
 tasks.register("echo", PnpmTask, { args: ["exec", "echo", "hello"] });
