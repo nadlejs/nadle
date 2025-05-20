c7d240f7280fa48683ee334a5b6074f604b88719
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-05-20 08:24:21 +0700

    2

--- build.nadle.ts
+++ build.nadle.ts
@@ -2,7 +2,7 @@ import { tasks, ExecTask, PnpmTask } from "nadle";
 
 tasks.register("clean", PnpmTask, { args: ["-r", "exec", "rimraf", "lib"] });
 
-tasks.register("spell", ExecTask, { command: "cspell", args: ["**", "--no-progress"] });
+tasks.register("spell", ExecTask, { command: "cspell", args: ["**", "--quiet", "--gitignore"] });
 tasks.register("eslint", PnpmTask, { args: ["eslint"] });
 tasks.register("prettier", ExecTask, { command: "prettier", args: ["--check", "."] });
 tasks.register("knip", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "knip"] });
