1a917a4bf1937a201a974036fde43beaa0b6691d
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-05-21 07:50:31 +0700

    1

--- build.nadle.ts
+++ build.nadle.ts
@@ -1,6 +1,6 @@
 import { tasks, ExecTask, PnpmTask } from "nadle";
 
-tasks.register("clean", PnpmTask, { args: ["-r", "exec", "rimraf", "lib", "build"] });
+tasks.register("clean", PnpmTask, { args: ["-r", "exec", "rimraf", "lib", "build"] }).config({ group: "Misc", description: "Clean build artifacts" });
 
 tasks.register("spell", ExecTask, { command: "cspell", args: ["**", "--quiet", "--gitignore"] });
 tasks.register("eslint", PnpmTask, { args: ["eslint"] });
@@ -8,12 +8,14 @@ tasks.register("prettier", ExecTask, { command: "prettier", args: ["--check", ".
 tasks.register("knip", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "knip"] });
 tasks.register("check").config({ dependsOn: ["spell", "eslint", "prettier", "knip"] });
 
-tasks.register("build", PnpmTask, { args: ["run", "-r", "build"] }).config({ dependsOn: ["check"] });
+tasks.register("compile", PnpmTask, { args: ["run", "-r", "compile"] });
 
-tasks.register("testUnit", PnpmTask, { args: ["run", "-r", "test"] }).config({ dependsOn: ["build"] });
+tasks.register("testUnit", PnpmTask, { args: ["run", "-r", "test"] }).config({ dependsOn: ["compile"] });
 tasks
 	.register("testAPI", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "api-extractor", "run"] })
-	.config({ dependsOn: ["build"] });
+	.config({ dependsOn: ["compile"] });
 tasks.register("test").config({ dependsOn: ["testUnit", "testAPI"] });
 
 tasks.register("updateAPI", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "api-extractor", "run", "--local"] });
+
+tasks.register("build").config({ dependsOn: ["check", "compile", "test"] });
