76cb55b280bfe07652f633a23dffc327b56599aa
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-05-30 22:33:25 +0700

    5

--- packages/nadle/test/options/log-level.test.ts
+++ packages/nadle/test/options/log-level.test.ts
@@ -3,11 +3,11 @@ import { it, describe } from "vitest";
 import { exec, expectPass } from "../setup/utils.js";
 
 describe("--log-level", () => {
-	it("shows info log when passing --log-level=info", async () => {
+	it.skip("shows info log when passing --log-level=info", async () => {
 		await expectPass(exec`hello --log-level=info`);
 	});
 
-	it("shows debug log when passing --log-level=debug", async () => {
+	it.skip("shows debug log when passing --log-level=debug", async () => {
 		await expectPass(exec`hello --log-level=debug`);
 	});
 
