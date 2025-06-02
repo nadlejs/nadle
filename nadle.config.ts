13ad220081bb024428502923472843b93e2f8bb5
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-02 07:57:26 +0700

    chore(deps-dev): bump nadle from 0.3.2 to 0.3.3 (#112)

--- nadle.config.ts
+++ nadle.config.ts
@@ -7,21 +7,19 @@ tasks
 tasks.register("spell", ExecTask, { command: "cspell", args: ["**", "--quiet", "--gitignore"] });
 tasks.register("eslint", PnpmTask, { args: ["-r", "exec", "eslint", "--quiet"] });
 tasks.register("prettier", ExecTask, { command: "prettier", args: ["--check", "."] });
-tasks.register("knip", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "knip"] });
-tasks.register("validate", ExecTask, { command: "tsx", args: ["./packages/validators/src/index.ts"] });
+tasks.register("knip", ExecTask, { args: [], command: "knip" }).config({ workingDir: "./packages/nadle" });
+tasks.register("validate", ExecTask, { command: "tsx", args: ["./src/index.ts"] }).config({ workingDir: "./packages/validators" });
 tasks.register("check").config({ dependsOn: ["spell", "eslint", "prettier", "knip", "validate"] });
 
 tasks.register("build", PnpmTask, { args: ["-r", "build"] });
 
 tasks.register("testUnit", PnpmTask, { args: ["run", "-r", "test"] }).config({ dependsOn: ["build"] });
-tasks
-	.register("testAPI", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "api-extractor", "run"] })
-	.config({ dependsOn: ["build"] });
+tasks.register("testAPI", ExecTask, { args: ["run"], command: "api-extractor" }).config({ dependsOn: ["build"], workingDir: "./packages/nadle" });
 tasks.register("test").config({ dependsOn: ["testUnit", "testAPI"] });
 
 tasks.register("fixEslint", PnpmTask, { args: ["-r", "exec", "eslint", "--quiet", "--fix"] });
 tasks.register("fixPrettier", ExecTask, { command: "prettier", args: ["--write", "."] });
 tasks.register("format").config({ dependsOn: ["fixEslint", "fixPrettier"] });
 tasks
-	.register("updateAPI", PnpmTask, { args: ["-r", "--filter", "./packages/nadle", "exec", "api-extractor", "run", "--local"] })
-	.config({ dependsOn: ["build"] });
+	.register("updateAPI", ExecTask, { command: "api-extractor", args: ["run", "--local"] })
+	.config({ dependsOn: ["build"], workingDir: "./packages/nadle" });
