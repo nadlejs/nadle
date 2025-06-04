a3ab99ed6066a9455f35062b9b1e117e13ae3826
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-04 21:45:33 +0700

    security: remove process.env logs more

--- packages/nadle/test/fixtures/main/nadle.env.ts
+++ packages/nadle/test/fixtures/main/nadle.env.ts
@@ -1,6 +1,6 @@
 import { tasks, type Task } from "nadle";
 
-tasks.register("firstTask", () => console.log(process.env)).config({ env: { FIRST_TASK_ENV: "first task env" } });
+tasks.register("firstTask", () => console.log({ FIRST_TASK_ENV: process.env.FIRST_TASK_ENV })).config({ env: { FIRST_TASK_ENV: "first task env" } });
 
 const MyTask: Task = {
 	run: () => {
