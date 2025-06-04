5cd7d0dd878b6fd2e8d2c7f29cb6092c68cb6efd
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-04 21:07:59 +0700

    fix wrong links

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -123,12 +123,6 @@ const config: Config = {
 			// Optional: see doc section below
 			contextualSearch: true,
 
-			// Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-			replaceSearchResultPathname: {
-				to: "/",
-				from: "/docs/" // or as RegExp: /\/docs\//
-			},
-
 			// Optional: Algolia search parameters
 			searchParameters: {},
 
