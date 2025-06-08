3cffc360877e971567387ae2b5647e272ad9882c
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-08 12:29:48 +0700

    1

--- packages/nadle/test/task-configs/env.test.ts
+++ packages/nadle/test/task-configs/env.test.ts
@@ -1,25 +1,25 @@
-import { getStdout, createExec } from "setup";
+import { NewExec } from "setup";
 import { it, expect, describe } from "vitest";
 
 describe("env", () => {
-	const exec = createExec({ config: "env" });
+	const exec = NewExec.createExec({ config: "env" });
 
 	it("can inject env to process.env from object config", async () => {
-		const stdout = await getStdout(exec`firstTask`);
+		const stdout = await NewExec.getStdout(exec`firstTask`);
 
 		expect(stdout).contain("first task env");
 		expect(stdout).not.contain("second task env");
 	});
 
 	it("can inject env to process.env from callback config", async () => {
-		const stdout = await getStdout(exec`secondTask`);
+		const stdout = await NewExec.getStdout(exec`secondTask`);
 
 		expect(stdout).contain("second task env");
 		expect(stdout).not.contain("first task env");
 	});
 
 	it("should not inject env from other task", async () => {
-		const stdout = await getStdout(exec`firstTask secondTask --sequence`);
+		const stdout = await NewExec.getStdout(exec`firstTask secondTask --sequence`);
 
 		expect(stdout.match(/first task env/g)).toHaveLength(1);
 		expect(stdout.match(/second task env/g)).toHaveLength(1);
@@ -28,14 +28,14 @@ describe("env", () => {
 	});
 
 	it("should not inject env from other task 2", async () => {
-		const stdout = await getStdout(exec`firstTask secondTask`);
+		const stdout = await NewExec.getStdout(exec`firstTask secondTask`);
 
 		expect(stdout.match(/first task env/g)).toHaveLength(1);
 		expect(stdout.match(/second task env/g)).toHaveLength(1);
 	});
 
 	it("should not inject env from other task 3", async () => {
-		const stdout = await getStdout(exec`firstTask secondTask --max-workers 2`);
+		const stdout = await NewExec.getStdout(exec`firstTask secondTask --max-workers 2`);
 
 		expect(stdout.match(/first task env/g)).toHaveLength(1);
 		expect(stdout.match(/second task env/g)).toHaveLength(1);
