fbf298b91396db0c71d8da163258929969d4f493
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-08 21:26:02 +0700

    chore: reduce default render interval and improve duration formatting (#153)

--- packages/nadle/src/core/utils.ts
+++ packages/nadle/src/core/utils.ts
@@ -12,7 +12,7 @@ export function capitalize(str: string): string {
 
 export function formatTime(time: number): string {
 	if (time > 1000) {
-		return `${(time / 1000).toFixed(2)}s`;
+		return `${Math.round(time / 1000)}s`;
 	}
 
 	return `${Math.round(time)}ms`;
