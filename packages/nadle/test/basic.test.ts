7b1178c886f96174a00cce5d7992911af5ac5596
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-11 10:26:37 +0700

    test: increase timeout for order tests in basic.test.ts

--- packages/nadle/test/basic.test.ts
+++ packages/nadle/test/basic.test.ts
@@ -70,13 +70,13 @@ describe("basic", () => {
 			expect(stdout).toRunInOrder("node", "install");
 		});
 
-		it("should run in order 7", { repeats: 3 }, async () => {
+		it("should run in order 7", { repeats: 3, timeout: 10000 }, async () => {
 			const stdout = await getStdout(exec`slow fast`);
 
 			expect(stdout).toRunInOrder("slow", "fast");
 		});
 
-		it("should run in order 8", { repeats: 3 }, async () => {
+		it("should run in order 8", { repeats: 3, timeout: 10000 }, async () => {
 			const stdout = await getStdout(exec`fast slow`);
 
 			expect(stdout).toRunInOrder("fast", "slow");
@@ -110,11 +110,11 @@ describe("basic", () => {
 			await expectPass(exec`task-B task-C --dry-run`);
 		});
 
-		it("should list tasks in order 7", { timeout: 10000 }, async () => {
+		it("should list tasks in order 7", async () => {
 			await expectPass(exec`task-A.1 task-B task-C --dry-run`);
 		});
 
-		it("should list tasks in order 8", { timeout: 10000 }, async () => {
+		it("should list tasks in order 8", async () => {
 			await expectPass(exec`task-B.2 task-A.1 task-B task-C --dry-run`);
 		});
 	});
