337edab99d4e488d9132a30818313aefac1026f4
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-06-18 21:59:32 +0700

    test: add project directory test for various package managers

---
+++ packages/nadle/test/features/project-dir.test.ts
@@ -0,0 +1,49 @@
+import * as Path from "node:path";
+
+import { getStdout } from "setup";
+import { it, expect, describe } from "vitest";
+import { createExec, fixturesDir } from "setup";
+
+describe("projectDir", () => {
+	const baseDir = Path.join(fixturesDir, "project-dir");
+
+	describe("given a package.json contains nadle.root = true", () => {
+		it("should traverse up to find the package", async () => {
+			const stdout = await getStdout(createExec({ cwd: Path.join(baseDir, "with-nadle-config", "sub-package", "src") })`--show-config`, {
+				serializeAll: true
+			});
+
+			expect(stdout).contain(`"projectDir": "/ROOT/test/__fixtures__/project-dir/with-nadle-config"`);
+		});
+	});
+
+	describe("given a npm monorepo", () => {
+		it("should traverse up to find the root package", async () => {
+			const stdout = await getStdout(createExec({ cwd: Path.join(baseDir, "with-npm", "sub-package", "src") })`--show-config`, {
+				serializeAll: true
+			});
+
+			expect(stdout).contain(`"projectDir": "/ROOT/test/__fixtures__/project-dir/with-npm"`);
+		});
+	});
+
+	describe("given a pnpm monorepo", () => {
+		it("should traverse up to find the root package", async () => {
+			const stdout = await getStdout(createExec({ cwd: Path.join(baseDir, "with-pnpm", "sub-package", "src") })`--show-config`, {
+				serializeAll: true
+			});
+
+			expect(stdout).contain(`"projectDir": "/ROOT/test/__fixtures__/project-dir/with-pnpm"`);
+		});
+	});
+
+	describe("given a yarn monorepo", () => {
+		it("should traverse up to find the root package", async () => {
+			const stdout = await getStdout(createExec({ cwd: Path.join(baseDir, "with-yarn", "sub-package", "src") })`--show-config`, {
+				serializeAll: true
+			});
+
+			expect(stdout).contain(`"projectDir": "/ROOT/test/__fixtures__/project-dir/with-yarn"`);
+		});
+	});
+});
