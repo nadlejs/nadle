4540a56d21375d54c299be940eb4b0fa6ff47aa0
Nam Hoang Le <namhoangle.dev@gmail.com>
2025-07-09 21:08:44 +0700

    knip

--- packages/nadle/src/core/utilities/constants.ts
+++ packages/nadle/src/core/utilities/constants.ts
@@ -18,7 +18,7 @@ export const RIGHT_ARROW = "→";
 export const VERTICAL_BAR = "|";
 
 export const PACKAGE_JSON = "package.json";
-export const BASE_CONFIG_FILE_NAME = "nadle.config";
+const BASE_CONFIG_FILE_NAME = "nadle.config";
 export const SUPPORT_EXTENSIONS = ["js", "mjs", "ts", "mts"];
 export const DEFAULT_CONFIG_FILE_NAMES = SUPPORT_EXTENSIONS.map((ext) => `${BASE_CONFIG_FILE_NAME}.${ext}`);
 export const CONFIG_FILE_PATTERN = `${BASE_CONFIG_FILE_NAME}.{${SUPPORT_EXTENSIONS.join(",")}}`;
--- packages/nadle/test/__setup__/constants.ts
+++ packages/nadle/test/__setup__/constants.ts
@@ -1,11 +1,9 @@
 import Path from "node:path";
 
-import { BASE_CONFIG_FILE_NAME } from "src/core/utilities/constants.js";
-
 import PackageJson from "../../package.json" with { type: "json" };
 
 const rootPackage = Path.resolve(import.meta.dirname, "..", "..");
 export const cliPath = Path.resolve(rootPackage, PackageJson.bin);
 export const fixturesDir = Path.resolve(rootPackage, "test", "__fixtures__");
 export const tempDir = Path.resolve(rootPackage, "test", "__temp__");
-export const defaultConfigFile = `${BASE_CONFIG_FILE_NAME}.ts`;
+export const defaultConfigFile = `nadle.config.ts`;
